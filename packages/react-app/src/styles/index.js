868718e7a43970672ad61be56faa941dd27d500f
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2022-09-25 20:32:56 +0800

    fixed lg and md screen width for exchange box

--- packages/react-app/src/styles/index.js
+++ packages/react-app/src/styles/index.js
@@ -1,16 +1,17 @@
 const styles = {
   // App.js
   container: "flex justify-center min-h-screen sm:px-16 px-6 bg-site-black",
-  innerContainer:
-    "flex justify-between items-center flex-col max-w-[1280px] w-full",
-  header: "flex flex-row justify-between items-center w-full sm:py-10 py-6",
-  exchangeContainer:
-    "flex-1 flex justify-start items-center flex-col w-full mt-0",
+
+  innerContainer: "flex justify-between items-center flex-col max-w-[1480px] w-full border border-dotted",
+
+  header: "flex flex-row justify-between items-center w-full sm:pt-10 py-6 border border-dotted",
+
+  exchangeContainer: "flex-1 flex justify-start items-center flex-col w-full mt-0 border border-dotted",
   headTitle: "text-white font-poppins font-black text-5xl tracking-wide",
   subTitle: "text-dim-white font-poppins font-medium mt-3 text-base",
+
   exchangeBoxWrapper: "mt-10 w-full flex justify-center",
-  exchangeBox:
-    "relative md:max-w-[700px] md:min-w-[500px] min-w-full max-w-full gradient-border p-[2px] rounded-3xl",
+  exchangeBox: "relative lg:max-w-[500px] xl:max-w-[700px] min-w-[400px] gradient-border p-[2px] rounded-3xl",
   exchange:
     "w-full min-h-[400px] bg-site-black backdrop-blur-[4px] rounded-3xl shadow-card flex p-10",
 
