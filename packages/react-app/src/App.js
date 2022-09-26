f6e596f23ce4fc9ec27f35227e81304f72ddca14
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2022-09-26 22:40:20 +0800

    UI design for the main page

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 import { useEthers } from "@usedapp/core";
 import styles from "./styles";
 import logo from "./assets/marvel_logo.png"
-import { Exchange, Loader, ToggleBar, WalletButton } from "./components";
+import { Exchange, Loader, WalletButton } from "./components";
 import { usePools } from "./hooks";
 import bg_1 from './assets/bg_1.jpg'
+import bg_2 from './assets/bg_2.jpg'
+import bg_3 from './assets/bg_3.jpg'
+import bg_4 from './assets/bg_4.jpg'
+import bg_5 from './assets/bg_5.jpg'
+import bg_6 from './assets/bg_6.jpg'
+import bg_7 from './assets/bg_7.jpg'
+import bg_8 from './assets/bg_8.jpg'
+import side_img from './assets/side_5.png'
 import { MdSwapVerticalCircle } from 'react-icons/md'
 import { FaChartPie } from 'react-icons/fa'
+import eye from './assets/eye_2.png'
 
 
 const App = () => {
@@ -29,35 +38,120 @@ const App = () => {
     setPoolOpen(true)
   }
 
-  const bgImage1 = {
+  window.addEventListener('mousemove', (e) => {
+    // console.log(e.clientX)
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const anchor = document.getElementById('anchor')
+    const rekt = anchor.getBoundingClientRect()
+    const anchorX = rekt.left + rekt.width / 2
+    const anchorY = rekt.top + rekt.height / 2
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
+    console.log(angleDeg)
+
+    const eyes = document.querySelectorAll('.eye')
+      eyes.forEach(eachEye => {
+        eachEye.style.transform = `rotate(${90 + angleDeg}deg)`
+      })
+  })
+
+  function angle(cx, cy, ex, ey) {
+    const dy = ey - cy
+    const dx = ex - cx
+    const rad = Math.atan2(dy, dx)
+    const deg = rad * 180 / Math.PI
+    return deg
+  }
+  const bgImage_1 = {
     backgroundImage: `url(${bg_1})`,
     height: '100vh',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat'
   }
 
+  const bgImage_2 = {
+    backgroundImage: `url(${bg_2})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_3 = {
+    backgroundImage: `url(${bg_3})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_4 = {
+    backgroundImage: `url(${bg_4})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_5 = {
+    backgroundImage: `url(${bg_5})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_6 = {
+    backgroundImage: `url(${bg_6})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_7 = {
+    backgroundImage: `url(${bg_7})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgImage_8 = {
+    backgroundImage: `url(${bg_8})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const bgArray = [bgImage_1, bgImage_2, bgImage_3, bgImage_4, bgImage_5, bgImage_6, bgImage_7, bgImage_8]
+
   return (
-    <div className="flex justify-center min-h-screen sm:px-16 px-6 bg-site-black" style={bgImage1}>
-      <div className="relative flex justify-between items-center flex-col max-w-[1480px] w-full">
+    <div 
+      className="relative flex justify-center min-h-screen sm:px-16 px-6 bg-site-black" 
+      style={bgArray[4]} 
+    >
+      <img src={side_img} alt="side-img" id="anchor" className="fixed hidden lg:block absolute w-[420px] -top-[10px] left-[30px] z-10 border" />
+      {/* <div className='hidden lg:block' id='eyes'>
+        <img src={eye} alt="eye" className='eye absolute w-[45px] lg:bottom-[180px] lg:right-[320px] xl:bottom-[200px] xl:right-[420px]' />
+        <img src={eye} alt="eye" className='eye absolute w-[45px] lg:bottom-[180px] lg:right-[160px] xl:bottom-[200px] xl:right-[220px]' />
+      </div> */}
+      <div className="relative flex justify-between items-center flex-col max-w-[1580px] w-full">
         {/* NAV BAR */}
         <header className="flex flex-row justify-between items-center w-full py-6">
           {/* logo + name */}
-          <div className="flex items-center w-full md:w-[20%] ">
+          <div className="flex items-center w-full md:w-[25%] z-20">
             <img
               src={logo}
               alt="uniswap-logo"
               className="xl:w-20 xl:h-20 w-16 h-16 object-contain"
             />
-            <h1 className="text-white ml-3 font-poppins font-black text-xl lg:text-2xl xl:text-3xl tracking-tight hidden sm:block">Marvel Swap 3.0</h1>
+            <h1 className="logo-text text-white ml-3 font-poppins font-black text-xl lg:text-3xl xl:text-4xl tracking-tight hidden lg:block">Marvel Swap</h1>
           </div>
           {/* toggle bar */}
-          <div className="hidden md:flex justify-center items-center w-[50%]">
+          <div className="hidden md:flex justify-center items-center w-[45%]">
             <div className='flex justify-between space-x-1 max-w-[60%] min-w-[400px] p-[6px] rounded-xl bg-site-black shadow
-            h-[3.5rem] xl:h-[3.6rem]'>
+            h-[3.5rem] xl:h-[3.6rem] shadow-xl'>
               <div
                 onClick={swapClick} 
                 className= {`duration-200 flex items-center group rounded-xl p-2 w-full cursor-pointer flex justify-center items-center space-x-1
-                    ${swapOpen ? 'bg-sky-500 w-[60%] text-gray-50' : 'text-gray-100 hover:bg-site-dim2 hover:text-gray-50 w-[40%]'}`}>
+                    ${swapOpen ? 'bg-sky-500 w-[50%] text-gray-50' : 'text-gray-100 hover:bg-site-dim2 hover:text-gray-50 w-[50%]'}`}>
                 <MdSwapVerticalCircle size={23} className="group-hover:rotate-180 duraing-500 transition-all" />
                 <h2 className='text-center font-mono font-bold'>Swap</h2>
 
@@ -65,7 +159,7 @@ const App = () => {
               <div
                 onClick={poolClick} 
                 className= {`duration-200 flex items-center group rounded-xl p-2 w-full cursor-pointer flex justify-center items-center space-x-2 
-                    ${poolOpen ? 'bg-sky-500 w-[60%] text-gray-50' : 'text-gray-100 hover:bg-site-dim2 hover:text-gray-50 w-[40%]'}`}>
+                    ${poolOpen ? 'bg-sky-500 w-[50%] text-gray-50' : 'text-gray-100 hover:bg-site-dim2 hover:text-gray-50 w-[50%]'}`}>
                 <FaChartPie size={23} className="group-hover:rotate-180 duraing-500 transition-all" />
                 <h2 className='text-center font-mono font-bold'>Pool</h2>
 
@@ -87,7 +181,7 @@ const App = () => {
 
           {/* EXCHANGE BOX */}
           {swapOpen && <div className="md:mt-16 mt-[8rem] w-full flex justify-center">
-            <div className="relative lg:max-w-[520px] xl:max-w-[700px] min-w-[450px] gradient-border p-[2px] rounded-3xl">
+            <div className="relative lg:max-w-[520px] xl:max-w-[700px] min-w-[450px] gradient-border p-[2px] rounded-3xl shadow-2xl">
               <div className="pink_gradient" />
               <div className="w-full min-h-[400px] bg-site-black backdrop-blur-[4px] rounded-3xl shadow-card flex px-6 py-10">
               {account ? (
